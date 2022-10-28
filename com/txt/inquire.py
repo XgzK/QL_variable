b76b1d3b101553997593864a684c2c1658cde356
XGZ <XGZ@com>
2022-10-28 18:47:19 +0800

    判断数据库返回类型避免数据库返回的是异常，查询数据库改成常量，尝试解决

--- com/txt/inquire.py
+++ com/txt/inquire.py
@@ -4,6 +4,8 @@ from com.gheaders import LoggerClass
 from com.sql import conn
 
 logger = LoggerClass('debug')
+# 读取数据库中活动全部链接的数据
+lines = conn.selectAll(table=conn.surface[0], where=f'jd_url != "" and jd_re != ""')
 
 
 def fuzzy_query(url=None):
@@ -13,28 +15,21 @@ def fuzzy_query(url=None):
     :return: 返回数据库 or []
     """
     try:
-        # 读取数据库中活动全部链接的数据
-        lines = conn.selectAll(table=conn.surface[0], where=f'jd_url != "" and jd_re != ""')
         # print(lines)
-        lis = []
+        li1s = []
         # 遍历数据库正则表达式非空
-        for i in lines:
-            try:
-                # print('----------------------------')
-                # print(i)
-                # print(url)
-                zzbds = re.findall(f'{i[6]}', url)
-                # print('匹配结果',zzbds)
-                if zzbds:
-                    # print('添加之前',lis)
-                    lis.append(i)
-                    # print('添加后',lis)
-            except Exception as e:
-                logger.write_log(f"异常的数据库值是: {i}")
-                logger.write_log(f"inquire.fuzzy_query 在对比数据库中出现异常: {e}")
-        if lis:
-            return lis
-        logger.write_log("模糊查询中: " + str(url) + " 没有找到,请添加")
+        if type(lines) == list:
+            for i in lines:
+                try:
+                    zzbds = re.findall(i[6], url)
+                    if zzbds:
+                        li1s.append(i)
+                except Exception as e:
+                    logger.write_log(f"异常的数据库值是: {i}")
+                    logger.write_log(f"inquire.fuzzy_query 在对比数据库中出现异常: {e}")
+            if li1s:
+                return li1s
+            logger.write_log("模糊查询中: " + str(url) + " 没有找到,请添加")
         return []
     except Exception as e:
         logger.write_log("inquire.fuzzy_query,异常问题: " + str(e) + "异常的值是: " + url)
