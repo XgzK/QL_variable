0454dd11d31b2d497cddafd342cb70c2c35865a2
XGZ <XGZ@com>
2022-11-20 14:22:01 +0800

    修复一些问题

--- com/txt/txt_compared.py
+++ com/txt/txt_compared.py
@@ -16,7 +16,7 @@ def tx_compared(tx1):
     try:
         print("进入对比数据方法: ", tx1)
         # 把export DPLHTY="b4be"的键和值分开
-        tx = re.findall('export (.*?)=(.*)', tx1)
+        tx = re.findall('(export .*?)=(.*)', tx1)
         # 如果分成两个尝试判断数据库中是否需要跳过去重复
         value1 = conn.selectTopone(table=conn.surface[0],
                                    where=f'jd_value1="NOT{tx[0][0]}" or jd_value1="{tx[0][0]}" '
@@ -24,12 +24,15 @@ def tx_compared(tx1):
                                          f'or jd_value3="{tx[0][0]}"')
         if value1 and value1[3] is None and value1[4] is None:
             q.put([value1[2], value1[3] + '=' + tx[0][0]])
+            tx1 = q.get()
+            print("队列执行: ", tx1)
+            main_core(tx1)
         elif value1:
             q.put([value1[2], tx1])
+            tx1 = q.get()
+            print("队列执行: ", tx1)
+            main_core(tx1)
         else:
             logger.write_log(f"在数据库中没有找到: {tx1}")
-        tx1 = q.get()
-        print("队列执行: ", tx1)
-        main_core(tx1)
     except Exception as e:
         logger.write_log(f"tx_compared 异常对比脚本异常信息信息: {e}")
