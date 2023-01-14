dfb46d4fe3f645fb2a81a2836e8a9a2396efd91b
XGZ <XGZ@com>
2023-01-14 21:07:37 +0800

    23/1/13 21:40

--- com/txt/inquire.py
+++ com/txt/inquire.py
@@ -181,7 +181,8 @@ class Conversion:
                                           where=f'jd_value1="NOT{tx[0][0]}" or jd_value1="{tx[0][0]}" '
                                                 f'or jd_value2="{tx[0][0]}" '
                                                 f'or jd_value3="{tx[0][0]}"')
-            if value1 and value1[3] is None and value1[4] is None:
+
+            if value1:
                 # [脚本, 活动, 时间]
                 q.put({
                     "jd_js": value1[2],
@@ -190,15 +191,7 @@ class Conversion:
                 })
                 logger.write_log(f"脚本名称 {value1[1]} 脚本 {value1[2]} 加入队列任务, 当前队列任务剩余 {q.qsize()} 个")
                 return
-            elif value1:
-                q.put({
-                    "jd_js": value1[2],
-                    "activities": value1[3] + '=' + tx[0][1],
-                    "interval": value1[10],
-                    "time": 0
-                })
-                logger.write_log(f"脚本 {value1[1]} 加入队列任务, 当前队列任务剩余 {q.qsize()} 个")
-                return
+
             else:
                 logger.write_log(f"在数据库中没有找到: {tx1}")
         except Exception as e:
